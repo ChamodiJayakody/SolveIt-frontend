b28616affe619a20f400f7a9c1b0c1b57f912f49
ChamodiJayakody <chamodijayakody@gmail.com>
2025-03-30 17:56:23 +0530

    dockerfile added

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,19 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3002,
+    host: true,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000', // Replace with your backend server URL
+      '/api/userService': {
+        target: 'http://localhost:3000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-})
+        rewrite: (path) => path.replace(/^\/api\/userService/, ''),
+      },
+      '/api/issueService': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/issueService/, ''),
+      },
+    },
+  },
+});
